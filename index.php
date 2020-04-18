42bce2f3892929a7e2a68b5b246499eb5a8e8b77
brijesh mandaliya <brij.mandaliya1998@gmail.com>
2020-04-18 20:09:14 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+</head>
+<body>
+<table border="1">
+<tr>
+<td rowspan="2">student</td>
+<td colspan="2">memo</td>
+<td rowspan="2">student</td>
+</tr>
+<tr>
+<td>First</td>
+<td>Second</td>
+</tr>
+<tr>
+<td>1</td>
+<td>1</td>
+<td>1</td>
+<td>1</td>
+</tr>
+<tr>
+<td>2</td>
+<td>2</td>
+<td>2</td>
+<td>2</td>
+</tr>
+<tr>
+<td>3</td>
+<td>3</td>
+<td>3</td>
+<td>3</td>
+</tr>
+</body>
+</html>
